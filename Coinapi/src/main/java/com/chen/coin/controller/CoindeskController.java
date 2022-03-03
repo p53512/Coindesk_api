713cc7e3bf08e1b021b0db97808879f6c22b93e0
TATPC009\Tatung <vans53512@gmail.com>
2022-03-03 13:39:59 +0800

    json

--- Coinapi/src/main/java/com/chen/coin/controller/CoindeskController.java
+++ Coinapi/src/main/java/com/chen/coin/controller/CoindeskController.java
@@ -7,7 +7,10 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,30 +31,38 @@ public class CoindeskController {
 	        Iterable<Coindesk> todoList = coindeskService.getcoindes();
 	        return todoList;
 	    }
-//	 public static void main(String[] args) throws IOException, JSONException {
-//		    JSONObject json = readJsonFromUrl("https://graph.facebook.com/19292868552");
-//		    System.out.println(json.toString());
-//		    System.out.println(json.get("id"));
-//		  }
-//	 public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException{
-//		InputStream is = new URL(url).openStream();
-//	    try {
-//	      BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-//	      String jsonText = readAll(rd);
-//	      JSONObject json = new JSONObject(jsonText);
-//	      return json;
-//	    } finally {
-//	      is.close();
-//	    }
-//	    
-//	    private static String readAll(Reader rd) throws IOException {
-//	        StringBuilder sb = new StringBuilder();
-//	        int cp;
-//	        while ((cp = rd.read()) != -1) {
-//	          sb.append((char) cp);
-//	        }
-//	        return sb.toString();
-//	      }
-//	}
+	 private static String readAll(Reader rd) throws IOException {
+		 StringBuilder sb = new StringBuilder();
+		 int cp;
+		 while ((cp = rd.read()) != -1) {
+			 sb.append((char) cp);
+		 }
+		 return sb.toString();
+	 }
+	 public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException{
+		 InputStream is = new URL(url).openStream();
+		 try {
+			 BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+			 String jsonText = readAll(rd);
+			 JSONObject json = new JSONObject(jsonText);
+			 return json;
+		 } finally {
+			 is.close();
+		 }
+		 
+	 }
+	 public static void main (String[] args)throws IOException {
+		    JSONObject json = readJsonFromUrl("https://api.coindesk.com/v1/bpi/currentprice.json");
+		    System.out.println(json.toString());
+		    System.out.println(json.getString("chartName"));
+		    System.out.println(json.getString("bpi"));
+		    Coindesk vo = new Coindesk();
+		    vo.setCoin(json.getString("chartName"));
+		    
+		    
+		    System.out.println(vo.getCoin());
+		  }
+	    
+	 
 	
 }
