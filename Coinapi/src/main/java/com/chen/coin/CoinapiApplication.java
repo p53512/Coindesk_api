1c2bc30c227b4d258750aed22c6d2610f190b7c9
TATPC009\Tatung <vans53512@gmail.com>
2022-03-03 11:29:04 +0800

    架構

---
+++ Coinapi/src/main/java/com/chen/coin/CoinapiApplication.java
@@ -0,0 +1,13 @@
+package com.chen.coin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinapiApplication.class, args);
+	}
+
+}
---
+++ Coinapi/src/test/java/com/chen/coin/CoinapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chen.coin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
